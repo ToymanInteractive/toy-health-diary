87d386537cba61ade5d60c6af69b0c1aa1ec77da
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-10-06 22:08:19 +0300

    build: add dart code metrics (#4)
    * build: add dart code metrics
    
    * chore: reformat code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:toy_health/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
