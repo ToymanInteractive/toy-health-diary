49a8ef6c63559ef8781bc5ac063529493b60e85f
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2022-01-01 16:05:36 +0300

    chore: update project

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"toy_health_diary", origin, size)) {
+  if (!window.CreateAndShow(L"Health diary", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
