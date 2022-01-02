d09f1306cacd59f17f7bf05d8d23297c945d5029
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2022-01-02 20:12:35 +0300

    fix tests

--- test/widgets/uikit/exercise_widget_test.dart
+++ test/widgets/uikit/exercise_widget_test.dart
@@ -5,8 +5,8 @@ import 'package:health_diary/widgets/uikit/circle_painter.dart';
 import 'package:health_diary/widgets/uikit/exercise_widget.dart';
 
 void main() {
-  group('Debug settings screen', () {
-    testGoldens('Exercise widget golden test', (tester) async {
+  group('Exercise widget', () {
+    testGoldens('golden test', (tester) async {
       final builder = GoldenBuilder.column()
         ..addScenario(
           'Exercise widget with image',
@@ -28,15 +28,45 @@ void main() {
       await screenMatchesGolden(tester, 'exercise_widget');
     });
 
-    testWidgets('widget test', (tester) async {
+    testWidgets('with image widget test', (tester) async {
       await tester.pumpWidgetBuilder(
-        const ExerciseWidget(size: 48),
+        const ExerciseWidget(
+          image: AssetImage('assets/stubs/exercise.png'),
+          size: 48,
+        ),
       );
 
       expect(
         find.byWidgetPredicate(
           (widget) =>
               widget is Image && widget.width == 42 && widget.height == 42,
+        ),
+        findsOneWidget,
+      );
+      expect(
+        find.byWidgetPredicate(
+          (widget) =>
+              widget is Padding && widget.padding == const EdgeInsets.all(3),
+        ),
+        findsOneWidget,
+      );
+      expect(
+        find.byWidgetPredicate(
+          (widget) =>
+              widget is CustomPaint &&
+              widget.painter is CirclePainter &&
+              (widget.painter as CirclePainter).width == 3,
+        ),
+        findsOneWidget,
+      );
+    });
+
+    testWidgets('with fallback icon widget test', (tester) async {
+      await tester.pumpWidgetBuilder(const ExerciseWidget(size: 48));
+
+      expect(
+        find.byWidgetPredicate(
+          (widget) => widget is Icon && widget.size == 31.5,
         ),
         findsOneWidget,
       );
